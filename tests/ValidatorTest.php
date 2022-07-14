3e3a1275e887387287e0e233a721baa47594f490
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2022-07-14 18:30:39 +0200

    Add Value and Validator tests

---
+++ tests/ValidatorTest.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+use Conia\Seiher\{Validator, Value};
+
+
+test('Validator validates', function () {
+    $validator = new Validator(
+        'same',
+        'Same',
+        function (Value $value, string $compare): bool {
+            return $value->value === $compare;
+        },
+        false
+    );
+
+    $value = new Value('testvalue', 'testvalue');
+    expect($validator->validate($value, 'testvalue'))->toBe(true);
+    $value = new Value('wrongvalue', 'wrongvalue');
+    expect($validator->validate($value, 'testvalue'))->toBe(false);
+    $value = new Value(null, null);
+    expect($validator->validate($value, 'testvalue'))->toBe(false);
+});
---
+++ tests/ValueTest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+use Conia\Seiher\Value;
+
+
+test('Properties numbers', function () {
+    $value = new Value(1, 2, null);
+
+    expect($value->value)->toBe(1);
+    expect($value->pristine)->toBe(2);
+    expect($value->error)->toBe(null);
+});
+
+
+test('Properties strings', function () {
+    $value = new Value('test1', 'test2', 'test3');
+
+    expect($value->value)->toBe('test1');
+    expect($value->pristine)->toBe('test2');
+    expect($value->error)->toBe('test3');
+});
+
+
+test('Properties arrays', function () {
+    $value = new Value([1, 2, 3], [2, 3, 4], [3, 4, 5]);
+
+    expect($value->value)->toBe([1, 2, 3]);
+    expect($value->pristine)->toBe([2, 3, 4]);
+    expect($value->error)->toBe([3, 4, 5]);
+});
