ce1c3f6ec14a2af2ae34343503b80a4ab87b70f2
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2022-07-15 08:56:44 +0200

    Remove constructor and add from SchemaInterface

--- src/SchemaInterface.php
+++ src/SchemaInterface.php
@@ -6,18 +6,6 @@ namespace Conia\Seiher;
 
 interface SchemaInterface
 {
-    public function __construct(
-        bool $list = false,
-        bool $keepUnknown = false,
-        array $langs = [],
-        ?string $title = null,
-    );
-    public function add(
-        string $field,
-        string $label,
-        string|SchemaInterface $type,
-        string ...$validators
-    ): void;
     public function validate(array $data, int $level = 1): bool;
     public function errors(bool $grouped = false): array;
     public function values(): array;
