d6ed7cd2719abab58802b53ccff6687d45a4845f
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2024-11-17 23:20:47 +0100

    Add docs dir to .php-cs-fixer.dist.php

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -2,10 +2,10 @@
 
 declare(strict_types=1);
 
-use PhpCsFixer\Finder;
 use FiveOrbs\Development\PhpCsFixer\Config;
+use PhpCsFixer\Finder;
 
-$finder = Finder::create()->in([__DIR__ . '/src', __DIR__ . '/tests']);
+$finder = Finder::create()->in([__DIR__ . '/src', __DIR__ . '/tests', __DIR__ . '/tests']);
 $config = new Config();
 
 return $config->setFinder($finder);
